9f279e721fa62a439d9ce0670cd17c20ec44cd1f
Bruna Terribile <brunaterribile@gmail.com>
2023-04-20 19:45:19 -0300

    feat: verify ticket/enrollm func + payment error

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,17 +1,21 @@
 import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
+import hotelsService from '@/services/hotels-service';
 
 export async function getAllHotels(req: AuthenticatedRequest, res: Response) {
   try {
     const { userId } = req;
-    const hotels = await hotelService.getHotels(Number(userId));
+    const hotels = await hotelsService.getHotels(Number(userId));
 
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
     if (error.name === 'NotFoundError') {
       return res.sendStatus(httpStatus.NOT_FOUND);
     }
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    if (error.name === 'PaymentRequired') {
+      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    }
+    return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 }
---
+++ src/services/hotels-service/index.ts
@@ -0,0 +1,28 @@
+import { TicketStatus } from '@prisma/client';
+import { notFoundError, paymentRequiredError } from '@/errors';
+import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
+
+async function verifyTicketAndEnrollment(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) throw notFoundError();
+
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
+
+  if (ticket.status == TicketStatus.RESERVED || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel)
+    throw paymentRequiredError();
+}
+
+async function getHotels(userId: number) {
+  await verifyTicketAndEnrollment(userId);
+
+  const hotels = await hotelsRepository.getAllHotels();
+  return hotels;
+}
+
+const hotelsService = {
+  getHotels,
+};
+
+export default hotelsService;
